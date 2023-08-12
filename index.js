9e6a671008f177ec25e3ee114f05d4508ce9df77
Bertin Selendo <contact@bertinselendo.com>
2023-08-12 02:40:44 +0100

    Page quote design and add button to generate quote without page reload

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ let Messages = [
         author: "Albert Einstein"
     },
     {
-        text: "Soyez qui vous êtes et dites ce que vous ressentez, parce que ceux qui s’en soucient n’ont pas d’importance, et ceux qui comptent ne s’en soucient pas.",
+        text: "Soyez qui vous êtes et dites ce que vous ressentez, parce que ceux qui s'en soucient n'ont pas d'importance, et ceux qui comptent ne s'en soucient pas.",
         author: "Mahatma Gandhi"
     },
     {
-        text: "Vous devez danser comme s’il n’y avait personne qui regardait, aimer comme si vous ne seriez jamais blessé, chanter comme s’il n’y avait personne qui écoutait, et vivre comme si c’était le paradis sur terre.",
+        text: "Vous devez danser comme s'il n'y avait personne qui regardait, aimer comme si vous ne seriez jamais blessé, chanter comme s'il n'y avait personne qui écoutait, et vivre comme si c'était le paradis sur terre.",
         author: "William W. Purkey"
     },
     {
@@ -31,7 +31,7 @@ let Messages = [
         author: "Frank Zappa"
     },
     {
-        text: "Un ami est quelqu’un qui sait tout de vous et qui vous aime toujours.",
+        text: "Un ami est quelqu'un qui sait tout de vous et qui vous aime toujours.",
         author: "Elbert Hubbard"
     }, 
     {
@@ -44,9 +44,6 @@ let Messages = [
     }, 
 ];
 
-/* Generate radom key */
-let key = Math.floor(Math.random() * 10);
-
 // console.log(Messages[key]);
 
 /**
@@ -54,8 +51,44 @@ let key = Math.floor(Math.random() * 10);
  */
 let quote = document.querySelector("#quote");
 let author = document.querySelector("#author");
-quote.innerHTML = Messages[key].text;
-author.innerHTML = Messages[key].author;
+
+function getQuote() {
+    /* Generate radom key */
+    let key = Math.floor(Math.random() * 10);
+    quote.innerHTML = Messages[key].text;
+    author.innerHTML = Messages[key].author;
+}
+
+window.onload = getQuote;
+
+let findQuote = document.querySelector("#findquote");
+let loader = document.querySelector("#loader");
+let bgImage = document.querySelector(".bgimage");
+
+function actionBefore() {
+    findQuote.style.opacity = "0.5";
+    quote.style.opacity = "0";
+    author.style.opacity = "0";
+    loader.style.display = "initial";
+    bgImage.classList.add('bgAnimate');
+}
+
+function actionAfter() {
+    findQuote.style.opacity = "1";
+    quote.style.opacity = "1";
+    author.style.opacity = "1";
+    loader.style.display = "none";
+    bgImage.classList.remove('bgAnimate');
+}
+
+findQuote.onclick = function(e) {
+    e.preventDefault();
+    actionBefore();
+    setTimeout(function() {
+        getQuote();
+        actionAfter();
+    }, 2000)
+}
 
 
 // Code is poetry
